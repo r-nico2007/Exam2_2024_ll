77b1081322be20ad59da6ba6d6a012c477ea8992
Nicolas Ramirez <nicolasramz216@gmail.com>
2024-10-07 20:47:54 -0500

    1

---
+++ Exam2_2024_ll/Program.cs
@@ -0,0 +1,35 @@
+﻿public class Program
+{
+    private static void Main(string[] args)
+    {
+        // Desarrollar el juego “Adivina el número” en un programa de C#. El juego es muy sencillo, consiste en 
+        //adivinar un número ℕ aleatorio que el sistema escoge de un determinado rango y lo guarda en
+        //memoria sin ser revelado. 
+
+        // El juego debe tener la funcionalidad de escoger el número de participantes que jugarán: mínimo 2 y
+        //máximo 4 integrantes.Dependiendo de la cantidad de jugadores, el número ℕ aleatorio se generará
+        //en los siguientes rangos:
+
+        //Si participan 2 jugadores, el número ℕ aleatorio se generará entre 0 y 50}
+        //Si participan 3 jugadores, el número ℕ aleatorio se generará entre 0 y 100 
+        //Si participan 4 jugadores, el número ℕ aleatorio se generará entre 0 y 200
+
+        //Cada jugador comenzará su turno intentando adivinar ese número ℕ aleatorio.El programa deberá
+        //mostrarle al jugador la siguiente información: 
+
+        // Si el número ingresado es mayor al número aleatorio, entonces mostrar por pantalla la
+        //palabra “MENOR” y darle la oportunidad al siguiente jugador.
+        // Si el número ingresado es menor al número aleatorio, entonces mostrar por pantalla la palabra 
+        // “MAYOR” y darle la oportunidad al siguiente jugador.
+        // Si el número ingresado coincide con el número aleatorio, entonces mostrar por pantalla la
+        // palabra “¡HAS GANADO!”. Aquí ya finaliza el juego.
+
+        // El programa deberá estar en capacidad de pedir a los jugadores si desean un nuevo “tirito” para
+        //volver a jugar y borrar consola, de lo contrario, finalizar el programa.
+
+        int numJugadores, numAletorio;
+        bool Juego = true;
+
+     
+    }
+}
